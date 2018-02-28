5533644ff6980b5aaeb6a4768812b15995aff834
thiagometal <thiagometal.fne@gmail.com>
2018-02-28 14:02:37 -0400

    adiciona controller de perfil

---
+++ aula4/app/Http/Controllers/PerfilController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Perfil;
+
+class PerfilController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Perfil::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        Perfil::create($request->all());
+        return $request->all();
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        Perfil::find($id)->update($request->all());
+        
+        return $request->all();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Perfil::find($id)->delete();
+        
+        return UserMetal::all();
+    }
+}
