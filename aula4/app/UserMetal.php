9c64b3daa8b420aabe60d4df9487b594ebbf8f39
thiagometal <thiagometal.fne@gmail.com>
2018-03-01 17:44:23 -0400

    adiciona ajustes para adequacao dos campos lat e long

--- aula4/app/UserMetal.php
+++ aula4/app/UserMetal.php
@@ -8,7 +8,7 @@ class UserMetal extends Model
 {
     protected $table = 'user_metal';
 
-    protected $fillable = ['name', 'favorite_band', 'perfil_id', 'foto'];
+    protected $fillable = ['name', 'favorite_band', 'perfil_id', 'foto', 'lat', 'long'];
     
     public function perfil() {
         return $this->belongsTo('App\Perfil');
