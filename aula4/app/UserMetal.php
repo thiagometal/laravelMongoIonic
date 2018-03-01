aac915de4c22e22c28c2c4323547e172f3e14ea4
thiagometal <thiagometal.fne@gmail.com>
2018-03-01 14:45:24 -0400

    adiciona ajustes para adequacao do campo foto

--- aula4/app/UserMetal.php
+++ aula4/app/UserMetal.php
@@ -8,7 +8,7 @@ class UserMetal extends Model
 {
     protected $table = 'user_metal';
 
-    protected $fillable = ['name', 'favorite_band', 'perfil_id'];
+    protected $fillable = ['name', 'favorite_band', 'perfil_id', 'foto'];
     
     public function perfil() {
         return $this->belongsTo('App\Perfil');
